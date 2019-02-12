960bd497a63d5bfadf53187acad1272d75766741
CharlesCZ <karakankaczyn@gmail.com>
2019-02-12 22:26:36 +0100

    converters with commanders

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -45,4 +45,9 @@ public class RecipeControllerTest {
         mockMvc.perform(get("/recipe/show/1")).andExpect(status().isOk()).
                 andExpect(view().name("recipe/show")).andExpect(model().attributeExists("recipe"));
     }
+
+
+
+
+
 }
